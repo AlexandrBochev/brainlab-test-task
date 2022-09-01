a5c1b576535cfbf776649ea64edd8ca0277fb95e
Alexandr Bochev <abochev@gmail.com>
2022-09-01 18:35:43 +0300

    industries block

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-const industriesActive = document.querySelector('.industries_link');
+const industriesActive1 = document.querySelector('.industries_link1');
+const industriesActive2 = document.querySelector('.industries_link2');
 
 const blockActive1 = document.querySelector('.block_active1');
+const blockActive2 = document.querySelector('.block_active2');
 
-industriesActive.addEventListener('click', () => {
-  blockActive1.style.visibility = 'visible';
+industriesActive1.addEventListener('click', () => {
   blockActive1.style.opacity = '1';
+  blockActive2.style.opacity = '0';
+});
+
+industriesActive2.addEventListener('click', () => {
+  blockActive1.style.opacity = '0';
+  blockActive2.style.opacity = '1';
 });
