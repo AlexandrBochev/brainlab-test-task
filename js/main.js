63d6089ce61a16a98a2f635e98e9b8cc8451d814
Alexandr Bochev <abochev@gmail.com>
2022-09-05 20:32:49 +0300

    mobile start

--- js/main.js
+++ js/main.js
@@ -1,128 +1,64 @@
-const industriesActive1 = document.querySelector('.industries_link1');
-const industriesActive2 = document.querySelector('.industries_link2');
-const industriesActive3 = document.querySelector('.industries_link3');
-const industriesActive4 = document.querySelector('.industries_link4');
-const industriesActive5 = document.querySelector('.industries_link5');
-const industriesActive6 = document.querySelector('.industries_link6');
-const industriesActive7 = document.querySelector('.industries_link7');
-const industriesActive8 = document.querySelector('.industries_link8');
-const industriesActive9 = document.querySelector('.industries_link9');
+// Slider
+$(document).ready(function(){
 
-const blockActive1 = document.querySelector('.industrie_block1');
-const blockActive2 = document.querySelector('.industrie_block2');
-const blockActive3 = document.querySelector('.industrie_block3');
-const blockActive4 = document.querySelector('.industrie_block4');
-const blockActive5 = document.querySelector('.industrie_block5');
-const blockActive6 = document.querySelector('.industrie_block6');
-const blockActive7 = document.querySelector('.industrie_block7');
-const blockActive8 = document.querySelector('.industrie_block8');
-const blockActive9 = document.querySelector('.industrie_block9');
+  $('.industries_blocks').slick({
+    arrows: false,
+    fade: true,
+  });
 
-industriesActive1.addEventListener('click', () => {
-  blockActive1.style.opacity = '1';
-  blockActive2.style.opacity = '0';
-  blockActive3.style.opacity = '0';
-  blockActive4.style.opacity = '0';
-  blockActive5.style.opacity = '0';
-  blockActive6.style.opacity = '0';
-  blockActive7.style.opacity = '0';
-  blockActive8.style.opacity = '0';
-  blockActive9.style.opacity = '0';
+  $('.industries_list .industries_item').click(function() {
+    var $this = $(this);
+    $('.industries_blocks').slick('slickGoTo', $this.data('index'))
+  });
+
+  $('.slider').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    variableWidth: true,
+    touchThreshold: 20,
+    asNavFor: '.slider-nav',
+  });
+
+  $('.slider-nav').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    variableWidth: true,
+    touchThreshold: 20,
+    asNavFor: '.slider',
+  });
+
 });
 
-industriesActive2.addEventListener('click', () => {
-  blockActive1.style.opacity = '0';
-  blockActive2.style.opacity = '1';
-  blockActive3.style.opacity = '0';
-  blockActive4.style.opacity = '0';
-  blockActive5.style.opacity = '0';
-  blockActive6.style.opacity = '0';
-  blockActive7.style.opacity = '0';
-  blockActive8.style.opacity = '0';
-  blockActive9.style.opacity = '0';
+// Popup
+const openPopUp = document.querySelector('.open_popup');
+const closePopUp = document.querySelector('.popup_close'); 
+const popUp = document.querySelector('.popup'); 
+const bgClosePopUp = document.querySelector('.popup_bg '); 
+const sendClose = document.querySelector('.popup_send '); 
+
+openPopUp.addEventListener('click', (e) => {
+  e.preventDefault()
+  popUp.style.opacity = '1';
+  popUp.style.visibility = 'visible';
+});
+closePopUp.addEventListener('click', (e) => {
+  e.preventDefault()
+  popUp.style.opacity = '0';
+  popUp.style.visibility = 'hidden';
+});
+bgClosePopUp.addEventListener('click', (e) => {
+  e.preventDefault()
+  popUp.style.opacity = '0';
+  popUp.style.visibility = 'hidden';
+});
+sendClose.addEventListener('click', (e) => {
+  e.preventDefault()
+  popUp.style.opacity = '0';
+  popUp.style.visibility = 'hidden';
 });
 
-industriesActive3.addEventListener('click', () => {
-  blockActive1.style.opacity = '0';
-  blockActive2.style.opacity = '0';
-  blockActive3.style.opacity = '1';
-  blockActive4.style.opacity = '0';
-  blockActive5.style.opacity = '0';
-  blockActive6.style.opacity = '0';
-  blockActive7.style.opacity = '0';
-  blockActive8.style.opacity = '0';
-  blockActive9.style.opacity = '0';
-});
 
-industriesActive4.addEventListener('click', () => {
-  blockActive1.style.opacity = '0';
-  blockActive2.style.opacity = '0';
-  blockActive3.style.opacity = '0';
-  blockActive4.style.opacity = '1';
-  blockActive5.style.opacity = '0';
-  blockActive6.style.opacity = '0';
-  blockActive7.style.opacity = '0';
-  blockActive8.style.opacity = '0';
-  blockActive9.style.opacity = '0';
-});
-
-industriesActive5.addEventListener('click', () => {
-  blockActive1.style.opacity = '0';
-  blockActive2.style.opacity = '0';
-  blockActive3.style.opacity = '0';
-  blockActive4.style.opacity = '0';
-  blockActive5.style.opacity = '1';
-  blockActive6.style.opacity = '0';
-  blockActive7.style.opacity = '0';
-  blockActive8.style.opacity = '0';
-  blockActive9.style.opacity = '0';
-});
-
-industriesActive6.addEventListener('click', () => {
-  blockActive1.style.opacity = '0';
-  blockActive2.style.opacity = '0';
-  blockActive3.style.opacity = '0';
-  blockActive4.style.opacity = '0';
-  blockActive5.style.opacity = '0';
-  blockActive6.style.opacity = '1';
-  blockActive7.style.opacity = '0';
-  blockActive8.style.opacity = '0';
-  blockActive9.style.opacity = '0';
-});
-
-industriesActive7.addEventListener('click', () => {
-  blockActive1.style.opacity = '0';
-  blockActive2.style.opacity = '0';
-  blockActive3.style.opacity = '0';
-  blockActive4.style.opacity = '0';
-  blockActive5.style.opacity = '0';
-  blockActive6.style.opacity = '0';
-  blockActive7.style.opacity = '1';
-  blockActive8.style.opacity = '0';
-  blockActive9.style.opacity = '0';
-});
-
-industriesActive8.addEventListener('click', () => {
-  blockActive1.style.opacity = '0';
-  blockActive2.style.opacity = '0';
-  blockActive3.style.opacity = '0';
-  blockActive4.style.opacity = '0';
-  blockActive5.style.opacity = '0';
-  blockActive6.style.opacity = '0';
-  blockActive7.style.opacity = '0';
-  blockActive8.style.opacity = '1';
-  blockActive9.style.opacity = '0';
-});
-
-industriesActive9.addEventListener('click', () => {
-  blockActive1.style.opacity = '0';
-  blockActive2.style.opacity = '0';
-  blockActive3.style.opacity = '0';
-  blockActive4.style.opacity = '0';
-  blockActive5.style.opacity = '0';
-  blockActive6.style.opacity = '0';
-  blockActive7.style.opacity = '0';
-  blockActive8.style.opacity = '0';
-  blockActive9.style.opacity = '1';
-});
 
