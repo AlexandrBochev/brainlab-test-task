bcfc636d0ebce16b587fd6ea8aa881413a027281
Alexandr Bochev <abochev@gmail.com>
2022-09-06 19:08:18 +0300

    task 1 fin

--- js/main.js
+++ js/main.js
@@ -32,18 +32,6 @@
 // Sliders
 $(document).ready(function(){
 
-  if (window.innerWidth > 767) {
-    $('.industries_blocks').slick({
-      arrows: false,
-      fade: true,
-    });
-  };
-
-  $('.industries_list .industries_item').click(function() {
-    var $this = $(this);
-    $('.industries_blocks').slick('slickGoTo', $this.data('index'))
-  });
-
   $('.slider').slick({
     arrows: false,
     autoplay: true,
@@ -93,5 +81,146 @@ sendClose.addEventListener('click', (e) => {
   popUp.style.visibility = 'hidden';
 });
 
+// Industrie Block
+const firstSlide = document.getElementById('first_slide');
+const industriesActive1 = document.getElementById('industries_item1');
+const industriesActive2 = document.getElementById('industries_item2');
+const industriesActive3 = document.getElementById('industries_item3');
+const industriesActive4 = document.getElementById('industries_item4');
+const industriesActive5 = document.getElementById('industries_item5');
+const industriesActive6 = document.getElementById('industries_item6');
+const industriesActive7 = document.getElementById('industries_item7');
+const industriesActive8 = document.getElementById('industries_item8');
+const industriesActive9 = document.getElementById('industries_item9');
 
+const article1 = document.getElementById('article1');
+const article2 = document.getElementById('article2');
+const article3 = document.getElementById('article3');
+const article4 = document.getElementById('article4');
+const article5 = document.getElementById('article5');
+const article6 = document.getElementById('article6');
+const article7 = document.getElementById('article7');
+const article8 = document.getElementById('article8');
+const article9 = document.getElementById('article9');
 
+$(document).ready(function(){
+  article6.classList.add('active');
+  firstSlide.style.background = '#fff';
+});
+
+industriesActive1.addEventListener('click', () => {
+  article1.classList.add('active')
+  article2.classList.remove('active')
+  article3.classList.remove('active')
+  article4.classList.remove('active')
+  article5.classList.remove('active')
+  article6.classList.remove('active')
+  article7.classList.remove('active')
+  article8.classList.remove('active')
+  article9.classList.remove('active')
+  firstSlide.style.background = 'linear-gradient(90.06deg, #FFFFFF 70.83%, rgba(255, 255, 255, 0.25) 99.97%)';
+});
+
+industriesActive2.addEventListener('click', () => {
+  article1.classList.remove('active')
+  article2.classList.add('active')
+  article3.classList.remove('active')
+  article4.classList.remove('active')
+  article5.classList.remove('active')
+  article6.classList.remove('active')
+  article7.classList.remove('active')
+  article8.classList.remove('active')
+  article9.classList.remove('active')
+  firstSlide.style.background = 'linear-gradient(90.06deg, #FFFFFF 70.83%, rgba(255, 255, 255, 0.25) 99.97%)';
+});
+
+industriesActive3.addEventListener('click', () => {
+  article1.classList.remove('active')
+  article2.classList.remove('active')
+  article3.classList.add('active')
+  article4.classList.remove('active')
+  article5.classList.remove('active')
+  article6.classList.remove('active')
+  article7.classList.remove('active')
+  article8.classList.remove('active')
+  article9.classList.remove('active')
+  firstSlide.style.background = 'linear-gradient(90.06deg, #FFFFFF 70.83%, rgba(255, 255, 255, 0.25) 99.97%)';
+});
+
+industriesActive4.addEventListener('click', () => {
+  article1.classList.remove('active')
+  article2.classList.remove('active')
+  article3.classList.remove('active')
+  article4.classList.add('active')
+  article5.classList.remove('active')
+  article6.classList.remove('active')
+  article7.classList.remove('active')
+  article8.classList.remove('active')
+  article9.classList.remove('active')
+  firstSlide.style.background = 'linear-gradient(90.06deg, #FFFFFF 70.83%, rgba(255, 255, 255, 0.25) 99.97%)';
+});
+
+industriesActive5.addEventListener('click', () => {
+  article1.classList.remove('active')
+  article2.classList.remove('active')
+  article3.classList.remove('active')
+  article4.classList.remove('active')
+  article5.classList.add('active')
+  article6.classList.remove('active')
+  article7.classList.remove('active')
+  article8.classList.remove('active')
+  article9.classList.remove('active')
+  firstSlide.style.background = 'linear-gradient(90.06deg, #FFFFFF 70.83%, rgba(255, 255, 255, 0.25) 99.97%)';
+});
+
+industriesActive6.addEventListener('click', () => {
+  article1.classList.remove('active')
+  article2.classList.remove('active')
+  article3.classList.remove('active')
+  article4.classList.remove('active')
+  article5.classList.remove('active')
+  article6.classList.add('active')
+  article7.classList.remove('active')
+  article8.classList.remove('active')
+  article9.classList.remove('active')
+  firstSlide.style.background = '#fff';
+});
+
+industriesActive7.addEventListener('click', () => {
+  article1.classList.remove('active')
+  article2.classList.remove('active')
+  article3.classList.remove('active')
+  article4.classList.remove('active')
+  article5.classList.remove('active')
+  article6.classList.remove('active')
+  article7.classList.add('active')
+  article8.classList.remove('active')
+  article9.classList.remove('active')
+  firstSlide.style.background = 'linear-gradient(90.06deg, #FFFFFF 70.83%, rgba(255, 255, 255, 0.25) 99.97%)';
+});
+
+industriesActive8.addEventListener('click', () => {
+  article1.classList.remove('active')
+  article2.classList.remove('active')
+  article3.classList.remove('active')
+  article4.classList.remove('active')
+  article5.classList.remove('active')
+  article6.classList.remove('active')
+  article7.classList.remove('active')
+  article8.classList.add('active')
+  article9.classList.remove('active')
+  firstSlide.style.background = 'linear-gradient(90.06deg, #FFFFFF 70.83%, rgba(255, 255, 255, 0.25) 99.97%)';
+});
+
+industriesActive9.addEventListener('click', () => {
+  article1.classList.remove('active')
+  article2.classList.remove('active')
+  article3.classList.remove('active')
+  article4.classList.remove('active')
+  article5.classList.remove('active')
+  article6.classList.remove('active')
+  article7.classList.remove('active')
+  article8.classList.remove('active')
+  article9.classList.add('active')
+  firstSlide.style.background = 'linear-gradient(90.06deg, #FFFFFF 70.83%, rgba(255, 255, 255, 0.25) 99.97%)';
+});
